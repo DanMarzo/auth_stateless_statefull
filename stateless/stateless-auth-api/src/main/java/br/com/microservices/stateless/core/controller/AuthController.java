a3359395e3cc41374edd3f1d60be8b30218c106a
DanMarzo <marzogildan@gmail.com>
2024-07-13 20:31:15 -0300

    Arrumando controller e uso de construtores com lombok

---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservices/stateless/core/controller/AuthController.java
@@ -0,0 +1,24 @@
+package br.com.microservices.stateless.core.controller;
+
+import br.com.microservices.stateless.core.dto.AuthRequest;
+import br.com.microservices.stateless.core.dto.TokenDTO;
+import br.com.microservices.stateless.core.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("login")
+    public TokenDTO login(@RequestBody AuthRequest request){
+        return authService.login(request);
+    }
+
+    @PostMapping("token/validate")
+    public TokenDTO tokenValidate(@RequestHeader String accessToken){
+        return authService.validateToken(accessToken);
+    }
+}
--- stateless/stateless-auth-api/src/main/java/br/com/microservices/stateless/core/service/AuthService.java
+++ stateless/stateless-auth-api/src/main/java/br/com/microservices/stateless/core/service/AuthService.java
@@ -9,39 +9,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
-
 @Service
 @AllArgsConstructor
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final UserRepository userRepository;
-    public AuthService(PasswordEncoder passwordEncoder, JwtService jwtService, UserRepository userRepository) {
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
-        this.userRepository = userRepository;
-    }
-    public TokenDTO login(AuthRequest request){
+
+    public TokenDTO login(AuthRequest request) {
         var user = userRepository.findByUsername(request.username())
                 .orElseThrow(() -> new ValidationException(("")));
-        var accessToken =  jwtService.createToken(user);
+        var accessToken = jwtService.createToken(user);
         validatePassword(request.password(), user.getPassword());
         return new TokenDTO(accessToken);
     }
 
-    public void validatePassword (String rawPassword,String encodedPassword){
-        if(!passwordEncoder.matches(rawPassword, encodedPassword)){
-            throw  new ValidationException("The password is incorrect");
+    public void validatePassword(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw new ValidationException("The password is incorrect");
         }
     }
 
-    public TokenDTO validateToken (String accessToken){
+    public TokenDTO validateToken(String accessToken) {
         validateExistingToken(accessToken);
         jwtService.validateAccessToken(accessToken);
-        return  new TokenDTO(accessToken);
+        return new TokenDTO(accessToken);
     }
-    private void validateExistingToken(String accessToken){
-        if(ObjectUtils.isEmpty(accessToken)){
+
+    private void validateExistingToken(String accessToken) {
+        if (ObjectUtils.isEmpty(accessToken)) {
             throw new ValidationException("The access token must be informed");
         }
     }
